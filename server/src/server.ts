14b6b28f7926b231d55c7f26aea231201d718aa0
anjiboss <nhu.nhu.face@gmail.com>
2023-04-03 12:54:00 +0900

    fix module alias problem

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,40 +1,40 @@
 import { PrismaClient } from "@prisma/client";
 import express from "express";
 import cors from "cors";
+import "module-alias/register";
 
 export const dbclient = new PrismaClient();
-
 import { authRouter } from "./routes/auth";
 import { userRouter } from "./routes/user";
 
 const PORT = process.env.PORT || 5000;
 
 async function main() {
-  const app = express();
-  app.use(express.json());
-  app.use(
-    cors({
-      origin: ["https://hoppscotch.io", "http://localhost:3000"],
-    })
-  );
-  app.use("/api/v1/auth", authRouter);
-  app.use("/api/v1/user", userRouter);
+    const app = express();
+    app.use(express.json());
+    app.use(
+        cors({
+            origin: ["https://hoppscotch.io", "http://localhost:3000"],
+        })
+    );
+    app.use("/api/v1/auth", authRouter);
+    app.use("/api/v1/user", userRouter);
 
-  app.get("/", (__, rs) => {
-    rs.json({
-      app: "running",
+    app.get("/", (__, rs) => {
+        rs.json({
+            app: "running",
+        });
     });
-  });
 
-  app.listen(PORT, () => {
-    console.log("running on port ", PORT);
-  });
+    app.listen(PORT, () => {
+        console.log("running on port ", PORT);
+    });
 }
 
 main()
-  .catch((e) => {
-    throw e;
-  })
-  .finally(async () => {
-    await dbclient.$disconnect();
-  });
+    .catch((e) => {
+        throw e;
+    })
+    .finally(async () => {
+        await dbclient.$disconnect();
+    });
